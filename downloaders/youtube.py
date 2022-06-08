953e9e984cf8a428a5ff5cb647e37230d724d6d6
ShailendraOP <91666183+ShailendraOP@users.noreply.github.com>
2022-06-09 03:14:20 +0530

    Add files via upload

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -1,1 +1,32 @@
+# 𝐃𝐎𝐍𝐓 𝐌𝐄𝐒𝐒 𝐖𝐈𝐓𝐇 𝐂𝐎𝐃𝐄𝐒 𝐂𝐎𝐏𝐘𝐑𝐈𝐆𝐇𝐓 @SHAILENDRA34 | @HYPER_AD13 | @ShiningOff
+# 𝐃𝐞𝐚𝐫 𝐏𝐞𝐫𝐨 𝐩𝐩𝐥𝐬 𝐏𝐥𝐢𝐬𝐡 𝐃𝐨𝐧'𝐭 𝐫𝐞𝐦𝐨𝐯𝐞 𝐭𝐡𝐢𝐬 𝐥𝐢𝐧𝐞 𝐟𝐫𝐨𝐦 𝐡𝐞𝐫𝐞 🌚
 
+
+from os import path
+
+from yt_dlp import YoutubeDL
+
+from config import BOT_NAME as bn, DURATION_LIMIT
+from helpers.errors import DurationLimitError
+
+ydl_opts = {
+    "format": "bestaudio/best",
+    "verbose": True,
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+ydl = YoutubeDL(ydl_opts)
+
+
+def download(url: str) -> str:
+    info = ydl.extract_info(url, False)
+    duration = round(info["duration"] / 60)
+
+    if duration > DURATION_LIMIT:
+        raise DurationLimitError(
+            f"🤐 ᴠɪᴅᴇᴏ ɪᴢ ʟᴏɴɢᴇʀ ᴛʜᴇɴ {DURATION_LIMIT} ᴍɪɴᴜᴛᴇ(s) ᴛʜᴀᴛ ᴀʀᴇɴ'ᴛ ᴀʟʟᴏᴡᴇᴅ, ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇᴅ ᴠɪᴅᴇᴏ ɪᴢ ᴀʙᴏᴜᴛ ᴛᴏ {duration} ᴍɪɴᴜᴛᴇ(s)"
+        )
+
+    ydl.download([url])
+    return path.join("downloads", f"{info['id']}.{info['ext']}")
