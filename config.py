3d9d5a6fb9667c265345494129ff2c7fa4894c24
ShailendraOP <91666183+ShailendraOP@users.noreply.github.com>
2022-06-09 07:57:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ que = {}
 admins = {}
 
 API_ID = int(getenv("API_ID", "8236321"))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+API_HASH = getenv("API_HASH", "974c0347e3de1ba02b540f07fb970e55")
+BOT_TOKEN = getenv("BOT_TOKEN", "2120048918:AAEqjhz1ZnI3mE6qeoT8auZSSSBHb470Hv8")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Shailendra34")
 BOT_USERNAME = getenv("BOT_USERNAME", "HeroMusics_Bot")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "Yaaro_ki_yaarii")
 BOT_NAME = getenv("BOT_NAME","𝐇𝐞𝐫𝐨 𝐌𝐮𝐬𝐢𝐜𝐬 𝐁𝐨𝐭")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
-SESSION_NAME = getenv("SESSION_NAME", None)
+SESSION_NAME = getenv("SESSION_NAME", "BQBaOfSGfUxWjUvZaUxZ4VD-KxoOTrj8jqUS2JPmE6Qv4KZEHVU4NN9zQNuVadgGcHcbzHhI4RtILWTpxBqfGmQDMPz59o8Sz-FgLsCs0i81p1kOiIpnSQ2wlfRdDL2FfiEYoKndYdaBZGIW-9-yz1ADi3zA_vpUxqgw0_KR82tANwg4Qer9SqKYPftK_VE_wfIkEI46_Tf-pT8EcwEgs0Kvv3AiSH_VJQPAtmt69XU-AO-rJDo50GRfiNKFKLIrq-QdrDQcjC-cLJDejyW-aWJmLKrPOUURad_O7PvoGhWpTi48_l2RBrlaf2t3ed5KEPtXARq4yGhmUz-kZE4hB9tEd3NCKAA")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 PMPERMIT = getenv("PMPERMIT", "DISABLE")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1323020756").split()))
