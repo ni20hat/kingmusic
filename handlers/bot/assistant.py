f80d57aab42d0680e6382f3cfb6e2346e87ef5ad
HariboTube <57761676+HariboTube@users.noreply.github.com>
2022-07-29 11:42:23 +0300

    Update assistant.py

--- handlers/bot/assistant.py
+++ handlers/bot/assistant.py
@@ -33,8 +33,8 @@ async def addchannel(client, message):
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🔵 Hata 🔵\n User {user.first_name} userbot için yoğun katılma istekleri nedeniyle grubunuza katılamadı! Asistanın grupta yasaklanmadığından emin olun."
-            "\n\n Yada Asistan Hesabını Gruba Kendin Ekle </b>",
+            f"<b>Asistan {user.first_name} için yoğun katılma istekleri nedeniyle grubunuza katılamadı! Asistanın grupta yasaklanmadığından emin olun."
+            "Veya Asistan Hesabını Gruba Kendin Ekle </b>",
         )
         return
     await message.reply_text(
